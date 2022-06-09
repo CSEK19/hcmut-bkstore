666e1151d877bd2ad67b57c0f83394046fc2c2df
Minhhung3815 <minhhungnguyen3815@gmail.com>
2022-06-09 21:04:38 +0700

    commit on 'main'

--- vnpay_return.php
+++ vnpay_return.php
@@ -11,7 +11,7 @@
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>Thông tin thanh toán</title>
+        <title>Billing Information</title>
         <!-- Bootstrap core CSS -->
         <link href="/vnpay_php/assets/bootstrap.min.css" rel="stylesheet"/>
         <!-- Custom styles for this template -->
@@ -51,41 +51,41 @@
         <!--Begin display -->
         <div class="container">
             <div class="header clearfix">
-                <h3 class="text-muted">Thông tin thanh toán</h3>
+                <h3 class="text-muted">Billing Information</h3>
             </div>
             <div class="table-responsive">
                 <div class="form-group">
-                    <label >Mã đơn hàng:</label>
+                    <label >Order Id:</label>
                     
                     <label><?php echo $_GET['vnp_TxnRef'] ?></label>
                 </div>    
                 <div class="form-group">
 
-                    <label >Số tiền:</label>
+                    <label >Price:</label>
                     <label><?=number_format($_GET['vnp_Amount']/100) ?> VNĐ</label>
                 </div>  
                 <div class="form-group">
-                    <label >Nội dung thanh toán:</label>
+                    <label >Billing content:</label>
                     <label><?php echo $_GET['vnp_OrderInfo'] ?></label>
                 </div> 
                 <div class="form-group">
-                    <label >Mã phản hồi (vnp_ResponseCode):</label>
+                    <label >Response Code:</label>
                     <label><?php echo $_GET['vnp_ResponseCode'] ?></label>
                 </div> 
                 <div class="form-group">
-                    <label >Mã GD Tại VNPAY:</label>
+                    <label >VNPAY Code:</label>
                     <label><?php echo $_GET['vnp_TransactionNo'] ?></label>
                 </div> 
                 <div class="form-group">
-                    <label >Mã Ngân hàng:</label>
+                    <label >Bank Id:</label>
                     <label><?php echo $_GET['vnp_BankCode'] ?></label>
                 </div> 
                 <div class="form-group">
-                    <label >Thời gian thanh toán:</label>
+                    <label >Date:</label>
                     <label><?php echo $_GET['vnp_PayDate'] ?></label>
                 </div> 
                 <div class="form-group">
-                    <label >Kết quả:</label>
+                    <label >Result:</label>
                     <label>
                         <?php
                             if ($_GET['vnp_ResponseCode'] == '00') {
@@ -136,16 +136,16 @@
                                 mysqli_close($conn);
                                 setcookie('cart', "", -60, '/');
                                 
-                                echo "GD Thanh cong";
+                                echo "Successful";
                             } else {
-                                echo "GD Khong thanh cong";
+                                echo "Something went wrong";
                             }
                         ?>
 
                     </label>
                     <br>
-                    <a href="http://localhost/Laptrinhweb/Home">
-                        <button>Quay lại</button>
+                    <a href="http://localhost/bkstore/Home">
+                        <button>Back to home</button>
                     </a>
                 </div> 
             </div>
@@ -153,7 +153,7 @@
                 &nbsp;
             </p>
             <footer class="footer">
-                <p>&copy; Quản lý Tiếng Anh 2020</p>
+                <p>&copy; English 2022</p>
             </footer>
         </div>  
     </body>
